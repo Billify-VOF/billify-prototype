2087bc8e775425b0e1cb3bfa9004c957f56df53d
Christian Vuye <christianvuye@Christians-MacBook-Pro.local>
2024-11-26 19:26:21 +0100

    Update settings file

--- backend/billify/settings.py
+++ backend/billify/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',  # CORS middleware
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middlewarse.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
