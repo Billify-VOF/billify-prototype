d962681ddb9982c1fdfaea0c55a7bbca74cc8b0e
= <solutions.provider.dev@gmail.com>
2025-03-11 13:50:59 +0530

    Update code in base.py file

--- backend/utils/base.py
+++ backend/utils/base.py
@@ -1,11 +1,20 @@
 from cryptography.fernet import Fernet
 import base64
 import json
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-# Generate a key (you should store this securely in your environment or configuration file)
-def generate_key():
-    return Fernet.generate_key()
 
+key = os.getenv('FERNET_KEY')
+if key is None:
+    raise ValueError("FERNET_KEY not found in the .env file!")
+
+# Ensure the key is valid (it should be a byte string)
+key = key.encode()
+
+# def generate_key():
+#     return Fernet.generate_key()
 # Encryption function
 def encrypt_token(token: str, key: bytes) -> str:
     fernet = Fernet(key)
@@ -16,6 +25,4 @@ def encrypt_token(token: str, key: bytes) -> str:
 def decrypt_token(encrypted_token: str, key: bytes) -> str:
     fernet = Fernet(key)
     decrypted_token = fernet.decrypt(encrypted_token.encode())  # Convert the encrypted token back to bytes
-    return decrypted_token.decode()  # Return the decrypted token as a string
-
-
+    return decrypted_token.decode()
