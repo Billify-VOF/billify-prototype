2debd6242280d77b0f44cdb4ac032ce0a2afef22
= <solutions.provider.dev@gmail.com>
2025-03-12 18:32:20 +0530

    Update the encrypt and decrypt function

--- utils/base.py
+++ utils/base.py
@@ -17,12 +17,43 @@ key = key.encode()
 #     return Fernet.generate_key()
 # Encryption function
 def encrypt_token(token: str, key: bytes) -> str:
-    fernet = Fernet(key)
-    encrypted_token = fernet.encrypt(token.encode())  # Convert the token to bytes before encryption
-    return encrypted_token.decode()  # Return the encrypted token as a string
+   
+    try:
+        if not token or not token.strip():
+            raise ValueError("Invalid token: Token cannot be empty or whitespace.")
+        fernet = Fernet(key)
+        encrypted_token = fernet.encrypt(token.encode())  # Convert the token to bytes before encryption
+        logger.info("Token successfully encrypted.")
+        return encrypted_token.decode()  # Return the encrypted token as a string
+    
+    except ValueError as ve:
+        logger.error(f"Invalid key provided for encryption: {str(ve)}")
+        raise ValueError("The encryption key provided is invalid.")
+    
+    except Exception as e:
+        logger.error(f"Error while encrypting token: {str(e)}")
+        raise Exception(f"Error while encrypting token: {str(e)}")
+
 
 # Decryption function
 def decrypt_token(encrypted_token: str, key: bytes) -> str:
-    fernet = Fernet(key)
-    decrypted_token = fernet.decrypt(encrypted_token.encode())  # Convert the encrypted token back to bytes
-    return decrypted_token.decode()
+    
+    try:
+        if not encrypted_token or not encrypted_token.strip():
+            raise ValueError("Invalid token: Token cannot be empty or whitespace.")
+        fernet = Fernet(key)
+        decrypted_token = fernet.decrypt(encrypted_token.encode())  # Convert the encrypted token back to bytes
+        logger.info("Token successfully decrypted.")
+        return decrypted_token.decode()
+    
+    except InvalidToken as it:
+        logger.error(f"Invalid token provided for decryption: {str(it)}")
+        raise InvalidToken("The encrypted token is invalid.")
+    
+    except ValueError as ve:
+        logger.error(f"Invalid key provided for decryption: {str(ve)}")
+        raise ValueError("The decryption key provided is invalid.")
+    
+    except Exception as e:
+        logger.error(f"Error while decrypting token: {str(e)}")
+        raise Exception(f"Error while decrypting token: {str(e)}")
