7e68b4c9212ad127cb29746119d895dad4979f5b
= <solutions.provider.dev@gmail.com>
2025-03-12 17:44:44 +0530

    Update the unique in accesstoken and refresh token

--- backend/token_manager/models.py
+++ backend/token_manager/models.py
@@ -4,8 +4,8 @@ from django.contrib.auth.models import User
 # Create your models here.
 class PontoToken(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    access_token = models.CharField(max_length=255, null=True, blank=True)
-    refresh_token = models.CharField(max_length=255, null=True, blank=True)
+    access_token = models.CharField(max_length=255, null=True, blank=True,unique=True)
+    refresh_token = models.CharField(max_length=255, null=True, blank=True,unique=True)
     expires_in = models.IntegerField(default=3600)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
