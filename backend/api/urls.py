c1de71b8b37ab1f5e49c8b161b8ed63dad46dcf0
Christian Vuye <christianvuye@Christians-MacBook-Pro.local>
2025-03-14 19:49:12 +0100

    Resolve merge conflicts with main branch

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,8 +2,12 @@
 
 from django.urls import path
 from api.views.invoice import InvoiceUploadView, InvoicePreviewView
-from token_manager.views import fetch_account_details
+from token_manager.views import fetch_account_details, ponto_login
+from api.views.auth import LoginView, LogoutView
+
 urlpatterns = [
+    path("auth/login/", LoginView.as_view(), name="login"),
+    path("auth/logout/", LogoutView.as_view(), name="logout"),
     path(
         'invoices/upload/',
         InvoiceUploadView.as_view(),
@@ -14,5 +18,6 @@ urlpatterns = [
         InvoicePreviewView.as_view(),
         name='invoice-preview'
     ),
+    path('ponto-login/', ponto_login, name='ponto_login'),
     path('accounts/', fetch_account_details, name='accounts'),
 ]
