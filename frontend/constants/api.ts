d7689ca0cbb60205fb78583c2c40362c7119818f
night-fury-796 <mosesmawuru3@gmail.com>
2025-03-31 13:07:44 -0400

    feat: Frontend OAuth2 Redirection & NextJS ESLint Setup

--- frontend/constants/api.ts
+++ frontend/constants/api.ts
@@ -1,3 +1,5 @@
+export const BACKEND_API_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
+
 export const Ponto_Connect_2_Options = {
   CLIENT_ID: process.env.NEXT_PUBLIC_IBANITY_CLIENT_ID,
   REDIRECT_URI: process.env.NEXT_PUBLIC_REDIRECT_URI,
