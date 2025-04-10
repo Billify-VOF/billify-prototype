a680de57ab623e0e45ed36fbdd5c8133b3e422e9
kunal kumar kushwaha <kunalkumar.kus@gmail.com>
2025-04-10 09:05:09 +0530

    update space configurations

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -175,14 +175,18 @@ AUTH_USER_MODEL = "infrastructure.Account"
 if not DEBUG:
     # Production storage settings (S3/MinIO)
     DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-    AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
-    AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")
-    AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME")
-    AWS_S3_ENDPOINT_URL = env("AWS_S3_ENDPOINT_URL")
+    AWS_ACCESS_KEY_ID = env("DIGITAL_OCEAN_SPACES_KEY") 
+    AWS_SECRET_ACCESS_KEY = env("DIGITAL_OCEAN_SPACES_SECRET") 
+    AWS_STORAGE_BUCKET_NAME = env("DIGITAL_OCEAN_SPACES_NAME") 
+    AWS_S3_ENDPOINT_URL ='https://billify-staging.sfo3.digitaloceanspaces.com'
+    AWS_S3_OBJECT_PARAMETERS = {
+        'CacheControl': 'max-age=86400',
+    }
     AWS_DEFAULT_ACL = None
     AWS_S3_FILE_OVERWRITE = False
     AWS_S3_VERIFY = True
     AWS_S3_SIGNATURE_VERSION = "s3v4"
+    AWS_LOCATION = "media"
 
 LOGGING = {
     "version": 1,
